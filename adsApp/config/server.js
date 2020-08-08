71dffaf00319f59c62b8f888d2e53f8eff8cae7f
delpers <c.boschat@protonmail.com>
2020-08-08 18:34:21 +0200

    Frist Commit

---
+++ adsApp/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ce1fe15b630e1b34e9dd7ad147cb65e5'),
+    },
+  },
+});
---
+++ adsApp/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '022469ee-3b93-4c92-9c36-cad430dd6037'
+};
