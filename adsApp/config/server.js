4194071c7785df0e7fa94a6e3909986d38d96c32
delpers <c.boschat@protonmail.com>
2020-08-08 18:57:25 +0200

    Config

--- adsApp/config/server.js
+++ adsApp/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', $PORT),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'ce1fe15b630e1b34e9dd7ad147cb65e5'),
